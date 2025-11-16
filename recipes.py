7b25c1cc5bfe6218543bccecf01da08ec5dc6d28
Lukas Bläsi <lukas.blaesi@students.fhnw.ch>
2025-11-16 15:22:41 +0000

    Test

--- recipes.py
+++ recipes.py
@@ -7,3 +7,5 @@ def list_recipes(recipes):
     """Namen + Portionen anzeigen (Reihenfolge = aktuelle Listenreihenfolge)."""
     for i, r in enumerate(recipes):
         print(f"{i+1}. {r['name']} (Portionen: {r['portions']})")
+
+    return recipes
