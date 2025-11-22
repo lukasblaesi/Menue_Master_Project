94c7d7866b85c039a891160dfe62cb3396c6b901
Lukas Bläsi <lukas.blaesi@students.fhnw.ch>
2025-11-22 08:30:40 +0000

    Test

--- storage.py
+++ storage.py
@@ -17,15 +17,14 @@ def load_data():
     if not os.path.exists(DATA_FOLDER):
         os.makedirs(DATA_FOLDER)
 
-    recipes = _load_or_empty(FILE_RECIPES)   
-    weekplan = _load_or_empty(FILE_WEEKPLAN)  
+    recipes = _load_or_empty(FILE_RECIPES)
+    weekplan = _load_or_empty(FILE_WEEKPLAN)
 
     return recipes, weekplan
 
 
-def save_data(recipes, weekplan):
+def save_data(weekplan):
     """Speichert die Daten zurück in JSON-Dateien."""
-    #_save(FILE_RECIPES, recipes)
     _save(FILE_WEEKPLAN, weekplan)
 
 
@@ -38,6 +37,7 @@ def _load_or_empty(path):
         print(f"Folgendes file konnte nicht gefunden werden: {path}")
 
 # Eventuell löschen
+
 
 def _save(path, data):
     """Schreibt JSON zurück auf die Festplatte."""
